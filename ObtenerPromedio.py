8d89c23d9a12464dfcbab6367feca5dfd6d8cc42
Gustavo Lozada <gustavo@huperoon.net>
2024-07-11 18:54:27 -0500

    ejercicio completo

---
+++ ObtenerPromedio.py
@@ -0,0 +1,11 @@
+# Elaborar un algoritmo que 
+# solicite 2 números y muestre
+# el promedio de ambos
+
+N1 = int(input("Ingrese el número 1: "))
+N2 = int(input("Ingrese el número 2: "))
+
+S = N1 + N2
+P = S / 2
+
+print(P)
