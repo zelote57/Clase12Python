c9462a18971bfac57ba16e48ec14694bff3b899a
Gustavo Lozada <gustavo@huperoon.net>
2024-07-16 21:37:53 -0500

    km

---
+++ ConvertirKilometros.py
@@ -0,0 +1,10 @@
+# Escribe un programa que solicite al usuario ingresar la cantidad de 
+# kilómetros recorridos por una motocicleta y la cantidad de litros de 
+# combustible que consumió durante ese recorrido.
+# Mostrar el consumo de combustible por kilómetro.
+
+km = float(input("Ingrese la cantidad en kilometros: "))
+ltrs = float(input("Ingrese la cantidad en litros "))
+
+consumo = ltrs/km
+print(consumo)
