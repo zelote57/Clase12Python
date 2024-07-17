0337d542e28e1f3e97d3d15a4aa7f863868b8186
Gustavo Lozada <gustavo@huperoon.net>
2024-07-16 19:23:20 -0500

    serie fibonacci

---
+++ SerieFibonaccy.py
@@ -0,0 +1,17 @@
+# Realice un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+#  * La serie Fibonacci se compone por una sucesión de números en la que 
+# el siguiente siempre es la suma de los dos anteriores.
+#  * 0, 1, 1, 2, 3, 5, 8, 13...
+
+N = 13
+A = 21
+C=0
+
+for C in range (50):
+    print(N)
+    S=N+A
+    N=A
+    A=S
+
+
+
