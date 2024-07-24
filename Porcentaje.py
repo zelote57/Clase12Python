f23960c1356bf9b0d6071ac74e79696d04e3ca33
Gustavo Lozada <gustavo@huperoon.net>
2024-07-23 20:00:45 -0500

    add porcentaje

---
+++ Porcentaje.py
@@ -0,0 +1,22 @@
+# Escribe un programa que solicite al usuario
+# un número y le reste el 15%, almacenando 
+# todo en una única variable. A continuación, 
+# mostrar el resultado final en pantalla.
+
+N=float(input("Ingrese valor: "))
+N=(N-(N*0.15))
+print(N)
+
+# numero = int(input("escriba un número: "))
+# print(numero - (numero*15)/100)
+
+# R1 = N * 15
+# R2 = R1 / 100
+
+# RF = N - R2
+
+# print( RF )
+
+# RP = (N * 15) / 100
+# R = N - RP
+# print( R )
