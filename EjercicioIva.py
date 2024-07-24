3640e22542c1d9b1b6432deeb60295140b1d2e46
Gustavo Lozada <gustavo@huperoon.net>
2024-07-23 19:07:10 -0500

    ejercicios

---
+++ EjercicioIva.py
@@ -0,0 +1,6 @@
+import numpy
+
+valor = float(input("Ingrese valor a calcular iva "))
+print("tu valor es ", valor)
+conIva = valor * numpy.float32(1.15)
+print("Con iva", conIva)
---
+++ PromedioTresNumeros.py
@@ -0,0 +1,11 @@
+# Escriba un programa que solicite al usuario ingresar tres números
+# para luego mostrarle el promedio de los tres.
+
+N1 = int(input("ingrese n1 "))
+N2 = int(input("ingrese n2 "))
+N3 = int(input("ingrese n3 "))
+
+suma = N1 + N2 + N3
+promedio = suma / 3
+print(suma)
+print(promedio)
